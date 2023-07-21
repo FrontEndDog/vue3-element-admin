d8a0f4824ad8a55291e294ab7137b8fb5975f2e0
一只前端汪 <985313519@qq.com>
2023-07-21 18:00:19 +0800

    添加：登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="h-full bg-gradient-to-b from-cyan-100 to-blue-500">
-    <div class="text-3xl font-bold underline">登录</div>
+  <div class="flex h-full items-center justify-center bg-gradient-to-b from-white to-blue-300 pb-[20vh]">
+    <div class="w-96 rounded-2xl bg-white px-5 pb-5 pt-8">
+      <el-form size="large">
+        <el-form-item>
+          <div class="mx-auto text-center text-xl font-bold text-blue-500">vue3-element-admin</div>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="account" placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="password" placeholder="密码" password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="mx-auto" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineOptions({
   name: 'Login',
 })
+const account = ref('')
+const password = ref('')
 </script>
